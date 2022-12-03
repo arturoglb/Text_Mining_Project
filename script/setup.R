63d0ba24ec1051ef0454efc7eec709866b95601d
arturoglb <arturo.glb@hotmail.com>
2022-12-03 20:10:29 +0100

    Adding structure of project, web scraped data from amazon, and csv data

---
+++ script/setup.R
@@ -0,0 +1,55 @@
+
+
+#############################################
+## The following loads the needed packages ##
+#############################################
+
+# load the required packages
+packages <- c(
+  "here", # for the project's organization
+  "tidyverse", "tidytext", "wordcloud", "lexicon", "quanteda", "quanteda.textstats",
+  "tm", "ggrepel", "udpipe", "widyr", "textdata", "reshape2",  # for wrangling
+  "rvest", "magick", "pdftools", # web scrapping
+  "patchwork", "broom", "ggwordcloud", "igraph", # for plotting
+  "knitr", "kableExtra", "bookdown", "rmarkdown", "bslib", "summarytools",
+  "flextable", # for the report
+  "readxl", "readr", # read files
+  "irlba", "sentimentr", "quanteda.textmodels", "quanteda.textplots",
+  "ranger", "caret", "text2vec", "stringi", "jsonlite", "reticulate", "spacyr", # for analysis
+  "rio" # data import/export
+)
+
+purrr::walk(packages, library, character.only = TRUE) 
+
+######################################################
+## The following sets a few option for nice reports ##
+######################################################
+
+# general options
+options(
+  digits = 3,
+  str = strOptions(strict.width = "cut"),
+  width = 69,
+  tibble.width = 69,
+  cli.unicode = FALSE
+)
+
+# ggplot options
+theme_set(theme_light())
+
+# knitr options
+opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  # cache = TRUE,
+  # fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
+  # dpi = 300,
+  # out.width = "70%",
+  fig.align = "center",
+  # fig.width = 6,
+  # fig.asp = 0.618,
+  fig.show = "hold",
+  message = FALSE,
+  echo = FALSE,
+  warning = FALSE
+)
